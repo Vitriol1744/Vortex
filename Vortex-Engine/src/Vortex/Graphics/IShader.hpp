8e78949909d78410b26eca24578824f16a4d0cb6
Vitriol1744 <hesher-3000@wp.pl>
2021-07-14 20:49:10 +0200

    Added Shaders

---
+++ Vortex-Engine/src/Vortex/Graphics/IShader.hpp
@@ -0,0 +1,30 @@
+//
+// Created by vitriol1744 on 14.07.2021.
+//
+#pragma once
+
+#include "Core/Core.hpp"
+
+#include <string_view>
+
+namespace Vortex::Graphics
+{
+    class VT_API IShader
+    {
+        public:
+            virtual ~IShader() noexcept = default;
+
+            virtual void Bind() const noexcept = 0;
+            virtual void Unbind() const noexcept = 0;
+
+            virtual void Reload(std::string_view vertexPath, std::string_view pixelPath, bool precompiled) noexcept = 0;
+            virtual void Load(std::string_view vertexPath, std::string_view pixelPath, bool precompiled) noexcept = 0;
+
+            virtual void SetUniform2f(std::string_view name, Math::Vec2 vec) const noexcept = 0;
+            virtual void SetUniform3f(std::string_view name, Math::Vec3 vec) const noexcept = 0;
+            virtual void SetUniform4f(std::string_view name, Math::Vec4 vec) const noexcept = 0;
+            virtual void SetMat4f(std::string_view name, Math::Mat4 mat) const noexcept = 0;
+
+            static Ref<IShader> Create(std::string_view vertexPath, std::string_view pixelPath, bool precompiled = true);
+    };
+}
