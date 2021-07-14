2c1e368c42a0eb967f0f1a5b7b965233de850eac
Vitriol1744 <hesher-3000@wp.pl>
2021-07-14 20:48:29 +0200

    Loaded new GL functions

---
+++ Vortex-Engine/src/Vortex/Graphics/OpenGL46/OpenGL.cpp
@@ -0,0 +1,115 @@
+//
+// Created by vitriol1744 on 11.07.2021.
+//
+#include "OpenGL.hpp"
+
+namespace Vortex::Graphics
+{
+    #pragma region glFunctions
+    PFNGLATTACHSHADERPROC               glAttachShader              = nullptr;
+    PFNGLBINDBUFFERPROC                 glBindBuffer                = nullptr;
+    PFNGLBINDVERTEXARRAYPROC            glBindVertexArray           = nullptr;
+    PFNGLBUFFERDATAPROC                 glBufferData                = nullptr;
+    PFNGLCREATEBUFFERSPROC              glCreateBuffers             = nullptr;
+    PFNGLCREATEPROGRAMPROC              glCreateProgram             = nullptr;
+    PFNGLCREATESHADERPROC               glCreateShader              = nullptr;
+    PFNGLCREATEVERTEXARRAYSPROC         glCreateVertexArrays        = nullptr;
+    PFNGLCOMPILESHADERPROC              glCompileShader             = nullptr;
+    PFNGLDELETEBUFFERSPROC              glDeleteBuffers             = nullptr;
+    PFNGLDELETEPROGRAMPROC              glDeleteProgram             = nullptr;
+    PFNGLDELETESHADERPROC               glDeleteShader              = nullptr;
+    PFNGLDETACHSHADERPROC               glDetachShader              = nullptr;
+    PFNGLENABLEVERTEXATTRIBARRAYPROC    glEnableVertexAttribArray   = nullptr;
+    PFNGLGETPROGRAMINFOLOGPROC          glGetProgramInfoLog         = nullptr;
+    PFNGLGETPROGRAMIVPROC               glGetProgramiv              = nullptr;
+    PFNGLGETSHADERINFOLOG               glGetShaderInfoLog          = nullptr;
+    PFNGLGETSHADERIVPROC                glGetShaderiv               = nullptr;
+    PFNGLGETUNIFORMLOCATIONPROC         glGetUniformLocation        = nullptr;
+    PFNGLLINKPROGRAMPROC                glLinkProgram               = nullptr;
+    PFNGLSHADERBINARYPROC               glShaderBinary              = nullptr;
+    PFNGLSHADERSOURCEPROC               glShaderSource              = nullptr;
+    PFNGLSPECIALIZESHADERPROC           glSpecializeShader          = nullptr;
+    PFNGLUNIFORM2FPROC                  glUniform2f                 = nullptr;
+    PFNGLUNIFORM3FPROC                  glUniform3f                 = nullptr;
+    PFNGLUNIFORM4FPROC                  glUniform4f                 = nullptr;
+    PFNGLUNIFORMMATRIX4FVPROC           glUniformMatrix4fv          = nullptr;
+    PFNGLUSEPROGRAMPROC                 glUseProgram                = nullptr;
+    PFNGLVALIDATEPROGRAMPROC            glValidateProgram           = nullptr;
+    PFNGLVERTEXATTRIBDIVISORPROC        glVertexAttribDivisor       = nullptr;
+    PFNGLVERTEXATTRIBIPOINTERPROC       glVertexAttribIPointer      = nullptr;
+    PFNGLVERTEXATTRIBPOINTERPROC        glVertexAttribPointer       = nullptr;
+    #pragma endregion
+    static bool initialized = false;
+
+    #ifdef VT_PLATFORM_WINDOWS
+    #define ubyte(name)
+    void* GetProcAddress(const char* name)
+    {
+        void* p = (void*)wglGetProcAddress(name);
+        if(p == 0 || (p == (void*)0x1) || (p == (void*)0x2) || (p == (void*)0x3) || (p == (void*)-1))
+        {
+            HMODULE module = LoadLibraryA("opengl32.dll");
+            p = (void *)GetProcAddress(module, name);
+        }
+
+        if (!p) initialized = false;
+        return p;
+    }
+    #elif defined(VT_PLATFORM_LINUX)
+    #define ubyte(name) reinterpret_cast<const unsigned char *>(name)
+    void* GetProcAddress(const unsigned char* name)
+    {
+        void* p = (void*)glXGetProcAddress(name);
+        if(p == nullptr || (p == (void*)0x1) || (p == (void*)0x2) || (p == (void*)0x3) || (p == (void*)-1))
+        {
+            initialized = false;
+        }
+
+        return p;
+    }
+    #endif
+
+    uint32 vao;
+
+    bool LoadGLFunctions()
+    {
+        if (initialized) return true;
+        initialized = true;
+
+        glAttachShader              = (PFNGLATTACHSHADERPROC)               GetProcAddress(ubyte("glAttachShader"               ));
+        glBindBuffer                = (PFNGLBINDBUFFERPROC)                 GetProcAddress(ubyte("glBindBuffer"                 ));
+        glBindVertexArray           = (PFNGLBINDVERTEXARRAYPROC)            GetProcAddress(ubyte("glBindVertexArray"            ));
+        glBufferData                = (PFNGLBUFFERDATAPROC)                 GetProcAddress(ubyte("glBufferData"                 ));
+        glCreateBuffers             = (PFNGLCREATEBUFFERSPROC)              GetProcAddress(ubyte("glCreateBuffers"              ));
+        glCreateProgram             = (PFNGLCREATEPROGRAMPROC)              GetProcAddress(ubyte("glCreateProgram"              ));
+        glCreateShader              = (PFNGLCREATESHADERPROC)               GetProcAddress(ubyte("glCreateShader"               ));
+        glCreateVertexArrays        = (PFNGLCREATEVERTEXARRAYSPROC)         GetProcAddress(ubyte("glCreateVertexArrays"         ));
+        glCompileShader             = (PFNGLCOMPILESHADERPROC)              GetProcAddress(ubyte("glCompileShader"              ));
+        glDeleteBuffers             = (PFNGLDELETEBUFFERSPROC)              GetProcAddress(ubyte("glDeleteBuffers"              ));
+        glDeleteProgram             = (PFNGLDELETEPROGRAMPROC)              GetProcAddress(ubyte("glDeleteProgram"              ));
+        glDeleteShader              = (PFNGLDELETESHADERPROC)               GetProcAddress(ubyte("glDeleteShaders"              ));
+        glDetachShader              = (PFNGLDETACHSHADERPROC)               GetProcAddress(ubyte("glDetachShader"               ));
+        glEnableVertexAttribArray   = (PFNGLENABLEVERTEXATTRIBARRAYPROC)    GetProcAddress(ubyte("glEnableVertexAttribArray"    ));
+        glGetProgramInfoLog         = (PFNGLGETPROGRAMINFOLOGPROC)          GetProcAddress(ubyte("glGetProgramInfoLog"          ));
+        glGetProgramiv              = (PFNGLGETPROGRAMIVPROC)               GetProcAddress(ubyte("glGetProgramiv"               ));
+        glGetShaderInfoLog          = (PFNGLGETSHADERINFOLOG)               GetProcAddress(ubyte("glGetShaderInfoLog"           ));
+        glGetShaderiv               = (PFNGLGETSHADERIVPROC)                GetProcAddress(ubyte("glGetShaderiv"                ));
+        glGetUniformLocation        = (PFNGLGETUNIFORMLOCATIONPROC)         GetProcAddress(ubyte("glGetUniformLocation"         ));
+        glLinkProgram               = (PFNGLLINKPROGRAMPROC)                GetProcAddress(ubyte("glLinkProgram"                ));
+        glShaderBinary              = (PFNGLSHADERBINARYPROC)               GetProcAddress(ubyte("glShaderBinary"               ));
+        glShaderSource              = (PFNGLSHADERSOURCEPROC)               GetProcAddress(ubyte("glShaderSource"               ));
+        glSpecializeShader          = (PFNGLSPECIALIZESHADERPROC)           GetProcAddress(ubyte("glSpecializeShader"           ));
+        glUniform2f                 = (PFNGLUNIFORM2FPROC)                  GetProcAddress(ubyte("glUniform2f"                  ));
+        glUniform3f                 = (PFNGLUNIFORM3FPROC)                  GetProcAddress(ubyte("glUniform3f"                  ));
+        glUniform4f                 = (PFNGLUNIFORM4FPROC)                  GetProcAddress(ubyte("glUniform4f"                  ));
+        glUniformMatrix4fv          = (PFNGLUNIFORMMATRIX4FVPROC)           GetProcAddress(ubyte("glUniformMatrix4fv"           ));
+        glUseProgram                = (PFNGLUSEPROGRAMPROC)                 GetProcAddress(ubyte("glUseProgram"                 ));
+        glValidateProgram           = (PFNGLVALIDATEPROGRAMPROC)            GetProcAddress(ubyte("glValidateProgram"            ));
+        glVertexAttribDivisor       = (PFNGLVERTEXATTRIBDIVISORPROC)        GetProcAddress(ubyte("glVertexAttribDivisor"        ));
+        glVertexAttribIPointer      = (PFNGLVERTEXATTRIBIPOINTERPROC)       GetProcAddress(ubyte("glVertexAttribIPointer"       ));
+        glVertexAttribPointer       = (PFNGLVERTEXATTRIBPOINTERPROC)        GetProcAddress(ubyte("glVertexAttribPointer"        ));
+
+        VT_CORE_LOG_TRACE("GL Functions Loaded!\n");
+        return initialized;
+    }
+}
--- Vortex-Engine/src/Vortex/Graphics/OpenGL46/OpenGL.hpp
+++ Vortex-Engine/src/Vortex/Graphics/OpenGL46/OpenGL.hpp
@@ -12,67 +12,130 @@
 #elif defined(VT_PLATFORM_LINUX)
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
-#include <GL/gl.h>
 #include <GL/glx.h>
 #endif
 
 #include <KHR/khrplatform.h>
 
-#pragma region glTypes
-using GLsizeiptr = khronos_ssize_t;
-#pragma endregion
-
-#pragma region glMacros
-#define GL_ARRAY_BUFFER 0x8892
-#define GL_STATIC_DRAW 0x88E4
-#pragma endregion
-
-#pragma region glFunctions
-using PFNGLBINDBUFFERPROC = void(*)(GLenum target, GLuint buffer);
-static PFNGLBINDBUFFERPROC glBindBuffer;
-
-using PFNGLBUFFERDATAPROC = void(*)(GLenum target, GLsizeiptr size, const GLvoid * data, GLenum usage);
-static PFNGLBUFFERDATAPROC glBufferData;
-
-using PFNGLCREATEBUFFERSPROC = void(*)(GLsizei n, GLuint* buffers);
-static PFNGLCREATEBUFFERSPROC glCreateBuffers;
-
-using PFNGLDELETEBUFFERSPROC = void(*)(GLsizei n, const GLuint* buffers);
-static PFNGLDELETEBUFFERSPROC glDeleteBuffers;
-#pragma endregion
-
-namespace Vortex
+namespace Vortex::Graphics
 {
+    #pragma region glTypes
+    using GLenum        = uint32;
+    using GLint         = int32;
+    using GLsizei       = int32;
+    using GLsizeiptr    = khronos_ssize_t;
+    using GLuint        = uint32;
+    using GLvoid        = void;
+    #pragma endregion
+
+    #pragma region glMacros
+    #define GL_ARRAY_BUFFER 0x8892
+    #define GL_STATIC_DRAW 0x88E4
+    #pragma endregion
+
+    #pragma region glFunctions
+    using PFNGLATTACHSHADERPROC = GLvoid(*)(GLuint program, GLuint shader);
+    extern PFNGLATTACHSHADERPROC glAttachShader;
+
+    using PFNGLBINDBUFFERPROC = GLvoid(*)(GLenum target, GLuint buffer);
+    extern PFNGLBINDBUFFERPROC glBindBuffer;
+
+    using PFNGLBINDVERTEXARRAYPROC = GLvoid(*)(GLuint array);
+    extern PFNGLBINDVERTEXARRAYPROC glBindVertexArray;
+
+    using PFNGLBUFFERDATAPROC = GLvoid(*)(GLenum target, GLsizeiptr size, const GLvoid * data, GLenum usage);
+    extern PFNGLBUFFERDATAPROC glBufferData;
+
+    using PFNGLCREATEBUFFERSPROC = GLvoid(*)(GLsizei n, GLuint* buffers);
+    extern PFNGLCREATEBUFFERSPROC glCreateBuffers;
+
+    using PFNGLCREATEPROGRAMPROC = GLuint(*)();
+    extern PFNGLCREATEPROGRAMPROC glCreateProgram;
+
+    using PFNGLCREATESHADERPROC = GLuint(*)(GLenum shaderType);
+    extern PFNGLCREATESHADERPROC glCreateShader;
+
+    using PFNGLCREATEVERTEXARRAYSPROC = GLvoid(*)(GLsizei n, GLuint* arrays);
+    extern PFNGLCREATEVERTEXARRAYSPROC glCreateVertexArrays;
+
+    using PFNGLCOMPILESHADERPROC = GLvoid(*)(GLuint shader);
+    extern PFNGLCOMPILESHADERPROC glCompileShader;
+
+    using PFNGLDELETEBUFFERSPROC = GLvoid(*)(GLsizei n, const GLuint* buffers);
+    extern PFNGLDELETEBUFFERSPROC glDeleteBuffers;
+
+    using PFNGLDELETEPROGRAMPROC = GLvoid(*)(GLuint program);
+    extern PFNGLDELETEPROGRAMPROC glDeleteProgram;
+
+    using PFNGLDELETESHADERPROC = GLvoid(*)(GLuint shader);
+    extern PFNGLDELETESHADERPROC glDeleteShader;
+
+    using PFNGLDETACHSHADERPROC = GLvoid(*)(GLuint program, GLuint shader);
+    extern PFNGLDETACHSHADERPROC glDetachShader;
+
+    using PFNGLENABLEVERTEXATTRIBARRAYPROC = GLvoid(*)(GLuint index);
+    extern PFNGLENABLEVERTEXATTRIBARRAYPROC glEnableVertexAttribArray;
+
+    using PFNGLGETPROGRAMINFOLOGPROC = GLvoid(*)(GLuint program, GLsizei maxLength, GLsizei* length, GLchar* infoLog);
+    extern PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog;
+
+    using PFNGLGETPROGRAMIVPROC = GLvoid(*)(GLuint program, GLenum pname, GLint* params);
+    extern PFNGLGETPROGRAMIVPROC glGetProgramiv;
+
+    using PFNGLGETSHADERINFOLOG = GLvoid(*)(GLuint shader, GLsizei maxLength, GLsizei* length, GLchar* infoLog);
+    extern PFNGLGETSHADERINFOLOG glGetShaderInfoLog;
+
+    using PFNGLGETSHADERIVPROC = GLvoid(*)(GLuint shader, GLenum pname, GLint* params);
+    extern PFNGLGETSHADERIVPROC glGetShaderiv;
+
+    using PFNGLGETUNIFORMLOCATIONPROC = GLint(*)(GLuint program, const GLchar* name);
+    extern PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation;
+
+    using PFNGLLINKPROGRAMPROC = GLvoid(*)(GLuint program);
+    extern PFNGLLINKPROGRAMPROC glLinkProgram;
+
+    using PFNGLSHADERBINARYPROC = GLvoid(*)(GLsizei count, const GLuint* shaders, GLenum binaryFormat, const GLvoid* binary, GLsizei length);
+    extern PFNGLSHADERBINARYPROC glShaderBinary;
+
+    using PFNGLSHADERSOURCEPROC = GLvoid(*)(GLuint shader, GLsizei count, const GLchar** string, const GLint* length);
+    extern PFNGLSHADERSOURCEPROC glShaderSource;
+
+    using PFNGLSPECIALIZESHADERPROC = GLvoid(*)(GLuint shader, const GLchar* entryPoint, GLuint numSpecializationConstants, const GLuint* constantIndex, const GLuint* pConstantValue);
+    extern PFNGLSPECIALIZESHADERPROC glSpecializeShader;
+
+    using PFNGLUNIFORM2FPROC = GLvoid(*)(GLint location, GLfloat v0, GLfloat v1);
+    extern PFNGLUNIFORM2FPROC glUniform2f;
+
+    using PFNGLUNIFORM3FPROC = GLvoid(*)(GLint location, GLfloat v0, GLfloat v1, GLfloat v2);
+    extern PFNGLUNIFORM3FPROC glUniform3f;
+
+    using PFNGLUNIFORM4FPROC = GLvoid(*)(GLint location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
+    extern PFNGLUNIFORM4FPROC glUniform4f;
+
+    using PFNGLUNIFORMMATRIX4FVPROC = GLvoid(*)(GLint location, GLsizei count, GLboolean transpose, const GLfloat* value);
+    extern PFNGLUNIFORMMATRIX4FVPROC glUniformMatrix4fv;
+
+    using PFNGLUSEPROGRAMPROC = GLvoid(*)(GLuint program);
+    extern PFNGLUSEPROGRAMPROC glUseProgram;
+
+    using PFNGLVALIDATEPROGRAMPROC = GLvoid(*)(GLuint program);
+    extern PFNGLVALIDATEPROGRAMPROC glValidateProgram;
+
+    using PFNGLVERTEXATTRIBDIVISORPROC = GLvoid(*)(GLuint index, GLuint divisor);
+    extern PFNGLVERTEXATTRIBDIVISORPROC glVertexAttribDivisor;
+
+    using PFNGLVERTEXATTRIBIPOINTERPROC = GLvoid(*)(GLuint index, GLint size, GLenum type, GLsizei stride, const GLvoid* pointer);
+    extern PFNGLVERTEXATTRIBIPOINTERPROC glVertexAttribIPointer;
+
+    using PFNGLVERTEXATTRIBPOINTERPROC = GLvoid(*)(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const GLvoid* pointer);
+    extern PFNGLVERTEXATTRIBPOINTERPROC glVertexAttribPointer;
+    #pragma endregion
+
     #ifdef VT_PLATFORM_WINDOWS
-    static void* GetProcAddress(const char* name)
-    {
-        void* p = (void*)wglGetProcAddress(name);
-        if(p == 0 || (p == (void*)0x1) || (p == (void*)0x2) || (p == (void*)0x3) || (p == (void*)-1))
-        {
-            HMODULE module = LoadLibraryA("opengl32.dll");
-            p = (void *)GetProcAddress(module, name);
-        }
-
-        return p;
-    }
+    void* GetProcAddress(const char* name);
     #elif defined(VT_PLATFORM_LINUX)
-    static void* GetProcAddress(const unsigned char* name)
-    {
-        void* p = (void*)glXGetProcAddress(name);
-        if(p == 0 || (p == (void*)0x1) || (p == (void*)0x2) || (p == (void*)0x3) || (p == (void*)-1))
-        {
-            //HMODULE module = LoadLibraryA("opengl32.dll");
-            //p = (void *)GetProcAddress(module, name);
-            VT_CORE_LOG_ERROR("Couldn't load GL function!");
-        }
-
-        return p;
-    }
+    void* GetProcAddress(const unsigned char* name);
     #endif
 
-    static bool LoadGLFunctions()
-    {
-
-        return true;
-    }
+    bool LoadGLFunctions();
 }
