e785a0baff1f7f3ac45c3a8c325090be3b9a1706
Vitriol1744 <hesher-3000@wp.pl>
2021-07-14 20:51:11 +0200

    Added new headers

---
+++ Vortex-Engine/src/Vortex/Graphics/Graphics.hpp
@@ -0,0 +1,11 @@
+//
+// Created by vitriol1744 on 12.07.2021.
+//
+#pragma once
+
+#include "IRendererAPI.hpp"
+#include "Graphics/IMesh.hpp"
+#include "Graphics/IVertexBuffer.hpp"
+#include "Graphics/VertexBufferLayout.hpp"
+#include "Graphics/IIndexBuffer.hpp"
+#include "Graphics/IShader.hpp"
