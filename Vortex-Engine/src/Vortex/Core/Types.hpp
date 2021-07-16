428674e5c0dbd9a8138cf73012c596467e3204da
Vitriol1744 <hesher-3000@wp.pl>
2021-07-16 04:47:29 +0200

    Refactored

--- Vortex-Engine/src/Vortex/Core/Types.hpp
+++ Vortex-Engine/src/Vortex/Core/Types.hpp
@@ -23,14 +23,14 @@ namespace Vortex
 
     template<typename T>
     using Scope = std::unique_ptr<T>;
-    template<typename T, typename ... Args>
+    template<typename T, typename... Args>
     inline constexpr Scope<T> CreateScope(Args&& ... args)
     {
         return std::make_unique<T>(std::forward<Args>(args)...);
     }
     template<typename T>
     using Ref = std::shared_ptr<T>;
-    template<typename T, typename ... Args>
+    template<typename T, typename... Args>
     inline constexpr Ref<T> CreateRef(Args&& ... args)
     {
         return std::make_shared<T>(std::forward<Args>(args)...);
