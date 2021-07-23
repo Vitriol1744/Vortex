b6cb91f2366e6a25c7242f12a78a2bb773b90546
Vitriol1744 <hesher-3000@wp.pl>
2021-07-23 23:30:06 +0200

    new features

--- Vortex-Engine/src/Vortex/Window/IWindow.hpp
+++ Vortex-Engine/src/Vortex/Window/IWindow.hpp
@@ -35,18 +35,23 @@ namespace Vortex
             virtual void Update() = 0;
             virtual void Present() = 0;
 
-            virtual Scope<Graphics::IGraphicsContext>& GetGraphicsContext() = 0;
-
-            VT_NODISCARD virtual bool IsOpen() const noexcept = 0;
+            virtual Graphics::IGraphicsContext* GetGraphicsContext() = 0;
+        
             VT_NODISCARD virtual bool IsKeyPressed(Input::KeyCode keycode) const noexcept = 0;
             VT_NODISCARD virtual bool IsMouseButtonPressed(Input::MouseCode mousecode) const noexcept = 0;
+            
+            VT_NODISCARD virtual bool IsOpen() const noexcept = 0;
+            VT_NODISCARD virtual Math::Vec2u Position() const noexcept = 0;
 
+            virtual void ShowCursor() const noexcept = 0;
             virtual void HideCursor() const noexcept = 0;
-            //virtual void SetFullscreen(bool fullscreen) const = 0;
-            //virtual void SetIcon(std::string_view path) const = 0;
+            virtual void SetFullscreen(bool fullscreen) const = 0;
+            virtual void SetIcon(std::string_view path) const = 0;
             virtual void SetTitle(std::string_view title) const noexcept = 0;
             virtual void SetTitle(std::wstring_view title) const noexcept = 0;
-            //virtual void SetVisible(bool visible = true) const = 0;
+            virtual void SetPosition(Math::Vec2u position) const { SetPosition(position.x, position.y); }
+            virtual void SetPosition(uint32 x, uint32 y) const = 0;
+            virtual void SetVisible(bool visible = true) const = 0;
 
             VT_NODISCARD static Ref<IWindow> CreateWindow(int width, int height, std::wstring_view title, Ref<IWindow> share = nullptr);
 
@@ -57,13 +62,13 @@ namespace Vortex
             Event<Input::KeyCode, Math::Vec2> keyReleasedEvent;
             // uint32 character
             Event<uint32> keyTypedEvent;
-
             // Vortex::Input::MouseCode mouseCode, Vortex::Math::Vec2 mousePosition
             Event<Input::MouseCode, Math::Vec2> mouseButtonPressedEvent;
             // Vortex::Input::MouseCode mouseCode, Vortex::Math::Vec2 mousePosition
             Event<Input::MouseCode, Math::Vec2> mouseButtonReleasedEvent;
             // Vortex::Math::Vec2 wheel, Vortex::Math::Vec2 mousePosition
             Event<Math::Vec2, Math::Vec2> mouseScrolledEvent;
+            // Vortex::Math::Vec2 mousePosition
             Event<Math::Vec2> mouseMovedEvent;
 
         private:
