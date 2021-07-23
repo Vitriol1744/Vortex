d4a24749b2761d778cc342c3adf69c7b2ff63870
Vitriol1744 <hesher-3000@wp.pl>
2021-07-23 23:29:49 +0200

    Refactored

--- Vortex-Engine/src/Vortex/Utility/TextureLoader.cpp
+++ Vortex-Engine/src/Vortex/Utility/TextureLoader.cpp
@@ -3,20 +3,77 @@
 //
 #include "TextureLoader.hpp"
 
+#include <fstream>
+
 namespace Vortex::Utility
 {
-    Ref<Pixel> TextureLoader::LoadTexture(std::string_view path)
+    namespace
+    {
+        #pragma pack(push, 1)
+        struct BMPHeader
+        {
+            int16 signature;
+            int32 size;
+            int16 reserved1;
+            int16 reserved2;
+            int32 offset;
+        };
+        
+        struct DIBHeader
+        {
+            int32 headersSize;
+            int32 width;
+            int32 height;
+            int16 colorPlanesCount;
+            int16 bitsPerPixel;
+            int32 compressionMethod;
+            int32 imageSize;
+            int32 horizontalResolution;
+            int32 verticalResolution;
+            int32 colorsCount;
+            int32 importantColors;
+        };
+    }
+    
+    Ref<Pixel[]> TextureLoader::LoadTexture(std::string_view path)
     {
         return LoadPNG(path);
     }
 
-    Ref<Pixel> TextureLoader::LoadBMP(std::string_view path)
+    Ref<Pixel[]> TextureLoader::LoadBMP(std::string_view path)
     {
-        VT_CORE_ASSERT_MSG(false, "BMP Files are not supported!");
-        return nullptr;
+        std::ifstream ifs(path.data(), std::ios::binary);
+        if (!ifs) VT_CORE_LOG_WARN("Failed to Open BMP file at \"{}\"", path);
+        
+        Ref<Pixel[]> pixels;
+        
+        BMPHeader bmpHeader;
+        DIBHeader dibHeader;
+        
+        ifs.read((char*)&bmpHeader, sizeof(bmpHeader));
+        ifs.read((char*)&dibHeader, sizeof(dibHeader));
+        if (bmpHeader.signature != 0x4D42)
+        {
+            VT_CORE_LOG_WARN("File at path: {} is not BMP file!", path.data());
+            return nullptr;
+        };
+        
+        pixels = CreateRef<Pixel[]>(dibHeader.imageSize);
+        ifs.seekg(bmpHeader.offset, std::ios_base::beg);
+    
+        // Channels have to be swapped because bitmap has BGR format.
+        uint8 temp = 0;
+        for (int64 i = 0; i < dibHeader.imageSize; i += 3)
+        {
+            temp          = pixels[i];
+            pixels[i]     = pixels[i + 2];
+            pixels[i + 2] = temp;
+        }
+        
+        return pixels;
     }
 
-    Ref<Pixel> TextureLoader::LoadPNG(std::string_view path)
+    Ref<Pixel[]> TextureLoader::LoadPNG(std::string_view path)
     {
         VT_CORE_ASSERT_MSG(false, "BMP Files are not supported!");
         return nullptr;
