6a207949a2a17ee692a3adfc2cd828789e44480e
Vitriol1744 <hesher-3000@wp.pl>
2021-08-13 13:59:52 +0200

    [Core] Replaced Own Math Library with GLM

--- Vortex-Engine/src/Vortex/Core/Math/Vector.hpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Vitriol1744 on 26.06.2021.
-//
-#pragma once
-
-#include "Vortex/Core/Math/Vector2.hpp"
-#include "Vortex/Core/Math/Vector3.hpp"
-#include "Vortex/Core/Math/Vec4.hpp"
