9ca654b6934459ab8e3db44329a7e3fe8f8bef97
Vitriol1744 <hesher-3000@wp.pl>
2021-08-18 14:51:16 +0200

    Cleanup

--- scripts/Linux-Setup.sh
+++ scripts/Linux-Setup.sh
@@ -1,2 +1,7 @@
-cd ../build && cmake ..
+cd ../Vortex-Engine/vendor && git clone https://github.com/fmtlib/fmt
+git clone https://github.com/kcat/openal-soft
+rm -rf OpenAL
+mv openal-soft OpenAL
+cd ../../build
+cmake ..
 make
