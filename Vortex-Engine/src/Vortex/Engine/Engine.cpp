7193cff5fc8e01f2ad6bf47cde7433361abd1a8d
Vitriol1744 <hesher-3000@wp.pl>
2021-08-21 20:03:14 +0200

    [Engine] Started Working on ImGui

--- Vortex-Engine/src/Vortex/Engine/Engine.cpp
+++ Vortex-Engine/src/Vortex/Engine/Engine.cpp
@@ -3,19 +3,20 @@
 //
 #include "Engine.hpp"
 
-#include "Vortex/Core/Time.hpp"
-#include "Vortex/Core/EventSystem.hpp"
 #include "Vortex/Audio/AudioManager.hpp"
-//TODO: Remove later!
-#include "Vortex/Graphics/API/OpenGL46/OpenGL.hpp"
-#include "Vortex/Utility/AudioLoader.hpp"
+
+#include "Vortex/Core/EventSystem.hpp"
 #include "Vortex/Core/Input/Gamepad.hpp"
+#include "Vortex/Core/Time.hpp"
 
-#include <imgui/backends/imgui_impl_opengl3.h>
-    #include "Vortex/Graphics/API/IRendererAPI.hpp"
+#include "Vortex/Engine/Application.hpp"
 
+//TODO: Remove later!
+#include "Vortex/Graphics/API/IRendererAPI.hpp"
+#include "Vortex/Graphics/ImGui/Backend_ImGui_OpenGL3.hpp"
+#include "Vortex/Graphics/ImGui/ImGui.hpp"
 
-#include <iostream>
+#include "Vortex/Utility/AudioLoader.hpp"
 
 namespace Vortex
 {
@@ -44,40 +45,11 @@ namespace Vortex
         for (int i = 0; i < arguments.size(); i++) VTCoreLogInfo("Arg[{}]: {}", i, arguments[i]);
         VTCoreLogInfo("");
 
-        ImGui::CreateContext();
-        ImGui::StyleColorsDark();
-
-        ImGuiIO& io = ImGui::GetIO();
-        io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;
-        io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
-
-        io.KeyMap[ImGuiKey_Tab] = 0;
-        io.KeyMap[ImGuiKey_LeftArrow] = 1;
-        io.KeyMap[ImGuiKey_RightArrow] = 2;
-        io.KeyMap[ImGuiKey_UpArrow] = 3;
-        io.KeyMap[ImGuiKey_DownArrow] = 4;
-        io.KeyMap[ImGuiKey_PageUp] = 5;
-        io.KeyMap[ImGuiKey_PageDown] = 6;
-        io.KeyMap[ImGuiKey_Home] = 7;
-        io.KeyMap[ImGuiKey_End] = 8;
-        io.KeyMap[ImGuiKey_Insert] = 9;
-        io.KeyMap[ImGuiKey_Delete] = 10;
-        io.KeyMap[ImGuiKey_Backspace] = 11;
-        io.KeyMap[ImGuiKey_Space] = 12;
-        io.KeyMap[ImGuiKey_Enter] = 13;
-        io.KeyMap[ImGuiKey_Escape] = 14;
-        io.KeyMap[ImGuiKey_KeyPadEnter] = 15;
-        io.KeyMap[ImGuiKey_A] = 16;
-        io.KeyMap[ImGuiKey_C] = 17;
-        io.KeyMap[ImGuiKey_V] = 18;
-        io.KeyMap[ImGuiKey_X] = 19;
-        io.KeyMap[ImGuiKey_Y] = 20;
-        io.KeyMap[ImGuiKey_Z] = 21;
-
-        ImGui_ImplOpenGL3_Init("#version 450");
+        Graphics::ImGuiRenderer::Initialize(window);
     }
     void Engine::Shutdown()
     {
+        delete app;
         AudioManager::Shutdown();
         Platform::Internal::Shutdown();
     }
@@ -120,60 +92,21 @@ namespace Vortex
 
     void Engine::Update()
     {
-        Graphics::IRendererAPI::Clear();
+        using namespace Graphics;
+        IRendererAPI::Clear();
         app->Update();
 
-        ImGuiIO& io = ImGui::GetIO();
-        io.DisplaySize = ImVec2(width, height);
-        io.DeltaTime = (float)Time::DeltaTime();
-
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui::NewFrame();
-
-        bool active = true;
-        ImGui::Begin("My First Tool", &active, ImGuiWindowFlags_MenuBar);
-        if (ImGui::BeginMenuBar())
-        {
-            if (ImGui::BeginMenu("File"))
-            {
-                if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */  }
-                if (ImGui::MenuItem("Save", "Ctrl+S"))   { /* Do stuff */  }
-                if (ImGui::MenuItem("Close", "Ctrl+W"))  { active = false; }
-                ImGui::EndMenu();
-            }
-            ImGui::EndMenuBar();
-        }
-
-        // Edit a color (stored as ~4 floats)
-        float color[]
-        {
-            0.0f,
-            1.0f,
-            1.0f,
-            1.0f
-        };
-        ImGui::ColorEdit4("Color", color);
-
-        // Plot some values
-        const float my_values[] = { 0.2f, 0.1f, 1.0f, 0.5f, 0.9f, 2.2f };
-        ImGui::PlotLines("Frame Times", my_values, IM_ARRAYSIZE(my_values));
-
-        // Display contents in a scrolling region
-        ImGui::TextColored(ImVec4(1,1,0,1), "Important Stuff");
-        ImGui::BeginChild("Scrolling");
-        for (int n = 0; n < 50; n++) ImGui::Text("%04d: Some text", n);
-        ImGui::EndChild();
-        ImGui::End();
-
+        ImGuiRenderer::Begin();
+        
         static bool show = true;
         ImGui::ShowDemoWindow(&show);
-
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        
+        ImGuiRenderer::End();
     }
 
     void Engine::Render()
     {
+        window->ActivateContext();
         app->Render();
     }
 }
