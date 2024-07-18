7b94b5e87634e976db358bd2bc60e8d900185e1c
V1tr10l7 <vitriol1744@gmail.com>
2024-07-18 14:47:11 +0200

    [feat]: add vortex version

---
+++ Vortex-Engine/src/Vortex/Core/Core.hpp
@@ -0,0 +1,18 @@
+/*
+ * Created by v1tr10l7 on 18.07.2024.
+ * Copyright (c) 2024-2024, Szymon Zemke <v1tr10l7@proton.me>
+ *
+ * SPDX-License-Identifier: GPL-3
+ */
+#pragma once
+
+#include "Vortex/Core/Assertions.hpp"
+#include "Vortex/Core/Export.hpp"
+#include "Vortex/Core/Log/Log.hpp"
+#include "Vortex/Core/Platform.hpp"
+#include "Vortex/Core/Types.hpp"
+
+namespace Vortex
+{
+    inline constexpr const Version version = {0, 1, 0};
+};
