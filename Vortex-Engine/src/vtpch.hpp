3de97cf2e85545840a41a10b4ebbeb08a7d4760c
V1tr10l7 <vitriol1744@gmail.com>
2024-07-26 03:54:28 +0200

    [feat]: add some usefull includes to precompiled header

--- Vortex-Engine/src/vtpch.hpp
+++ Vortex-Engine/src/vtpch.hpp
@@ -7,15 +7,34 @@
 #pragma once
 
 // Standard Library
+#include <algorithm>
 #include <any>
+#include <array>
+#include <chrono>
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
+#include <fstream>
 #include <functional>
 #include <iostream>
+#include <limits>
 #include <memory>
+#include <ostream>
 #include <queue>
+#include <set>
 #include <source_location>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
 
 // Dependencies
 #include <fmt/format.h>
 #include <magic_enum.hpp>
+
+#ifdef VT_EXPORT_VULKAN
+    #define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
+    #include <vulkan/vulkan.hpp>
+#endif
