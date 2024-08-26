168e02963ed9b2dfa4ab513a5824876151dc30a7
V1tr10l7 <vitriol1744@gmail.com>
2024-08-26 18:20:18 +0200

    [fix]: create build/Vortex-Engine directory before generating wayland headers

--- gen-wayland-headers.sh
+++ gen-wayland-headers.sh
@@ -3,5 +3,6 @@
 protocols_path=Vortex-Engine/vendor/wayland-protocols
 outpath=build/Vortex-Engine
 
+mkdir -p build/Vortex-Engine
 wayland-scanner public-code < $protocols_path/alpha-modifier-v1.xml > $outpath/wayland-alpha-modifier-v1-client-protocol.h
 wayland-scanner private-code < $protocols_path/alpha-modifier-v1.xml > $outpath/wayland-alpha-modifier-v1-protocol.c
