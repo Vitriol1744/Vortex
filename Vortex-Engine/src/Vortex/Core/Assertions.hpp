f121032c0b6f157921d7a47be34bb461efc2abea
V1tr10l7 <vitriol1744@gmail.com>
2024-08-27 04:22:23 +0200

    [fix]: cast expr to void as a workaround for 'result of comparision unused' warning in Release build

--- Vortex-Engine/src/Vortex/Core/Assertions.hpp
+++ Vortex-Engine/src/Vortex/Core/Assertions.hpp
@@ -85,12 +85,12 @@
         #define VtCoreSlowAssert(expr) VtCoreAssert(expr)
         #define VtSlowAssert(expr)     VtAssert(expr)
     #else
-        #define VtCoreSlowAssert(expr) ((void)expr)
-        #define VtSlowAssert(expr)     ((void)expr)
+        #define VtCoreSlowAssert(expr) ((void)(expr))
+        #define VtSlowAssert(expr)     ((void)(expr))
     #endif
 #else
-    #define VtCoreAssert(expr, msg)     ((void)expr)
-    #define VtAssert(expr, msg)         ((void)expr)
+    #define VtCoreAssert(expr, msg)     ((void)(expr))
+    #define VtAssert(expr, msg)         ((void)(expr))
     #define VtCoreSlowAssert(expr, msg) VtCoreAssert(expr, msg)
     #define VtSlowAssert(expr, msg)     VtAssert(expr, msg)
 #endif
