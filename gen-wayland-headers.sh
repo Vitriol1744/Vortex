366d7b60e8c182c9fb36fd02a715b8889c5604be
V1tr10l7 <vitriol1744@gmail.com>
2024-08-27 02:56:54 +0200

    [ci]: generate wayland headers

--- gen-wayland-headers.sh
+++ gen-wayland-headers.sh
@@ -3,6 +3,15 @@
 protocols_path=Vortex-Engine/vendor/wayland-protocols
 outpath=build/Vortex-Engine
 
+generate_header()
+{
+    protocol=$1
+    wayland-scanner public-code < $protocols_path/$protocol.xml > $outpath/wayland-"$protocol"-client-protocol.h
+}
+
 mkdir -p build/Vortex-Engine
-wayland-scanner public-code < $protocols_path/alpha-modifier-v1.xml > $outpath/wayland-alpha-modifier-v1-client-protocol.h
-wayland-scanner private-code < $protocols_path/alpha-modifier-v1.xml > $outpath/wayland-alpha-modifier-v1-protocol.c
+#wayland-scanner public-code < $protocols_path/alpha-modifier-v1.xml > $outpath/wayland-alpha-modifier-v1-client-protocol.h
+#wayland-scanner private-code < $protocols_path/alpha-modifier-v1.xml > $outpath/wayland-alpha-modifier-v1-protocol.c
+
+generate_header alpha-modifier-v1
+generate_header wayland
