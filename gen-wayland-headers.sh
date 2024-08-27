61bacef57f66bf7a06700f5d4727765f6f7b6a29
V1tr10l7 <vitriol1744@gmail.com>
2024-08-27 03:00:59 +0200

    [ci]: generate wayland missing headers

--- gen-wayland-headers.sh
+++ gen-wayland-headers.sh
@@ -13,5 +13,6 @@ mkdir -p build/Vortex-Engine
 #wayland-scanner public-code < $protocols_path/alpha-modifier-v1.xml > $outpath/wayland-alpha-modifier-v1-client-protocol.h
 #wayland-scanner private-code < $protocols_path/alpha-modifier-v1.xml > $outpath/wayland-alpha-modifier-v1-protocol.c
 
-generate_header alpha-modifier-v1
 generate_header wayland
+generate_header xdg-shell
+generate_header alpha-modifier-v1
