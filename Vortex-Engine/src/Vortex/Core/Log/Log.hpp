c272e413f1efeca724bfdf780022635a86b1104f
V1tr10l7 <vitriol1744@gmail.com>
2024-08-31 18:49:09 +0200

    [fix]: define VtCoreWarnOnce in release build

--- Vortex-Engine/src/Vortex/Core/Log/Log.hpp
+++ Vortex-Engine/src/Vortex/Core/Log/Log.hpp
@@ -77,6 +77,7 @@ namespace Vortex::Log
     #define VtCoreWarn(...)
     #define VtCoreError(...)
     #define VtCoreFatal(...)
+    #define VtCoreWarnOnce(...)
 
     #define VtTrace(...)
     #define VtInfo(...)
